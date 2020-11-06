c63d1b79c3dca179c265e99f15f732de859e46c7
Pawel Wieczorek <contact@cerberos.pl>
2020-11-06 21:06:16 +0100

    Change repository layout

---
+++ inc/dp.h
@@ -0,0 +1,15 @@
+#ifndef DP_H
+#define DP_H
+
+#include <iostream>
+#include <list>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+#include "brick.h"
+
+Tower get_highest_tower(std::istream &stream = std::cin);
+Tower get_highest_tower(std::vector<Brick> &bricks);
+
+#endif //BRICK_H
