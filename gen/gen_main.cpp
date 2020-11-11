274bb9c41f1922da35923ef177c7a2beaf32b23e
psawicki <01143606@pw.edu.pl>
2020-11-11 15:03:08 +0100

    better handling of input parameters for gen

--- gen/gen_main.cpp
+++ gen/gen_main.cpp
@@ -1,13 +1,24 @@
 #include "gen.h"
 
+const std::string AMOUNT_OPTION = "--amount";
+const std::string MIN_OPTION = "--min";
+const std::string MAX_OPTION = "--max";
+
 int main(int argc, char **argv) {
     int amount = 100;
-    double min = 1, max = 100;
-    if (argc > 1)
-        amount = atoi(argv[1]);
-    if (argc > 2)
-        min = std::stod(argv[2]);
-    if (argc > 3)
-        max = std::stod(argv[3]);
+    double min = 0.1, max = 100;
+    for (int i = 1; i + 1 < argc; i += 2) {
+        int j = i + 1;
+        if (argv[i] == AMOUNT_OPTION)
+            amount = atoi(argv[j]);
+        else if (argv[i] == MIN_OPTION)
+            min = std::stod(argv[j]);
+        else if (argv[i] == MAX_OPTION)
+            max = std::stod(argv[j]);
+        else {
+            std::cerr << "Usage:\n " << argv[0] << " [--amount N] [--min R] [--max R]\n";
+            exit(1);
+        }
+    }
     generate(amount, min, max);
 }
